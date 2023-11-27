901f09bddb304d1095c67ee3df6518ab6a4fb91a
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-28 00:41:08 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Write("RASM URLNI YUKLANG:");
+        string url = Console.ReadLine();
+        byte[] result = await PictureResult(url);
+        Console.WriteLine($"RAsm Xajmi:{result.Length/1024} kbyte");
+    }
+    static async Task<byte[]> PictureResult(string url)
+    {
+        using(HttpClient client = new HttpClient())
+        {
+            byte[] response = await client.GetByteArrayAsync(url);
+            return response;
+        }
+    }
+}
